16a2c6c1dc1c3ce640fb0565eabfeef1045e3c36
Tony ElHabr <anthonyelhabr@gmail.com>
2024-01-20 10:25:11 -0600

    package R file

---
+++ R/soccerplotR-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import cli
+#' @import ggpath
+#' @importFrom ggplot2 element_grob
+#' @import grid
+## usethis namespace: end
+NULL
