5f67d476635a33fbec0ee528728825a76c2b74a5
Tony ElHabr <anthonyelhabr@gmail.com>
2024-01-20 10:27:40 -0600

    data -> geom_soccer_logos

--- R/geom_soccer_logos.R
+++ R/geom_soccer_logos.R
@@ -5,14 +5,15 @@
 #'   team abbreviation. The latter can be checked with [`valid_team_names()`].
 #'
 #' @inheritParams ggplot2::geom_point
+#' @inheritParams valid_team_names
 #' @section Aesthetics:
 #' `geom_eng_logos()` understands the following aesthetics (required aesthetics are in bold):
 #' \itemize{
 #'   \item{**x**}{ - The x-coordinate.}
 #'   \item{**y**}{ - The y-coordinate.}
-#'   \item{**team_abbr**}{ - The team abbreviation. Should be one of [`valid_team_names()`]. The function tries to clean team names internally by calling [`soccerplotR::clean_team_abbrs()`].}
+#'   \item{**team_name**}{ - The team abbreviation. Should be one of [`valid_team_names()`]. The function tries to clean team names internally by calling [`clean_team_abbrs()`].}
 #'   \item{`alpha = NULL`}{ - The alpha channel, i.e. transparency level, as a numerical value between 0 and 1.}
-#'   \item{`colour = NULL`}{ - The image will be colorized with this colour. Use the special character `"b/w"` to set it to black and white. For more information on valid colour names in ggplot2 see <https://ggplot2.tidyverse.org/articles/ggplot2-specs.html?q=colour#colour-and-fill>}
+#'   \item{`colour = NULL`}{ - The image will be colorized with this colour. Use the special character `'b/w'` to set it to black and white. For more information on valid colour names in ggplot2 see <https://ggplot2.tidyverse.org/articles/ggplot2-specs.html?q=colour#colour-and-fill>}
 #'   \item{`angle = 0`}{ - The angle of the image as a numerical value between 0° and 360°.}
 #'   \item{`hjust = 0.5`}{ - The horizontal adjustment relative to the given x coordinate. Must be a numerical value between 0 and 1.}
 #'   \item{`vjust = 0.5`}{ - The vertical adjustment relative to the given y coordinate. Must be a numerical value between 0 and 1.}
@@ -27,7 +28,7 @@
 #' }
 #' @param ... Other arguments passed on to [ggplot2::layer()]. These are
 #'   often aesthetics, used to set an aesthetic to a fixed value. See the below
-#'   section "Aesthetics" for a full list of possible arguments.
+#'   section 'Aesthetics' for a full list of possible arguments.
 #' @return A ggplot2 layer ([ggplot2::layer()]) that can be added to a plot
 #'   created with [ggplot2::ggplot()].
 #' @export
@@ -36,23 +37,23 @@
 #' library(soccerplotR)
 #' library(ggplot2)
 #'
-#' team_abbr <- soccerplotR::valid_team_names()
+#' team_name <- soccerplotR::valid_team_names()
 #'
 #' df <- data.frame(
 #'   a = rep(1:6, 5),
 #'   b = sort(rep(1:5, 6), decreasing = TRUE),
-#'   teams = team_abbr
+#'   teams = team_name
 #' )
 #'
-#' # keep alpha == 1 for all teams including an "A"
-#' matches <- grepl("A", team_abbr)
+#' # keep alpha == 1 for all teams including an 'A'
+#' matches <- grepl('A', team_name)
 #' df$alpha <- ifelse(matches, 1, 0.2)
-#' # also set a custom fill colour for the non "A" teams
-#' df$colour <- ifelse(matches, NA, "gray")
+#' # also set a custom fill colour for the non 'A' teams
+#' df$colour <- ifelse(matches, NA, 'gray')
 #'
 #' # scatterplot of all logos
 #' ggplot(df, aes(x = a, y = b)) +
-#'   geom_eng_logos(aes(team_abbr = teams), width = 0.075) +
+#'   geom_eng_logos(aes(team_name = teams), width = 0.075) +
 #'   geom_label(aes(label = teams), nudge_y = -0.35, alpha = 0.5) +
 #'   theme_void()
 #'
@@ -60,7 +61,7 @@
 #' # please note that you have to add scale_alpha_identity() to use the alpha
 #' # values in your dataset!
 #' ggplot(df, aes(x = a, y = b)) +
-#'   geom_eng_logos(aes(team_abbr = teams, alpha = alpha), width = 0.075) +
+#'   geom_eng_logos(aes(team_name = teams, alpha = alpha), width = 0.075) +
 #'   geom_label(aes(label = teams), nudge_y = -0.35, alpha = 0.5) +
 #'   scale_alpha_identity() +
 #'   theme_void()
@@ -69,7 +70,7 @@
 #' # please note that you have to add scale_alpha_identity() as well as
 #' # scale_color_identity() to use the alpha and colour values in your dataset!
 #' ggplot(df, aes(x = a, y = b)) +
-#'   geom_eng_logos(aes(team_abbr = teams, alpha = alpha, colour = colour), width = 0.075) +
+#'   geom_eng_logos(aes(team_name = teams, alpha = alpha, colour = colour), width = 0.075) +
 #'   geom_label(aes(label = teams), nudge_y = -0.35, alpha = 0.5) +
 #'   scale_alpha_identity() +
 #'   scale_color_identity() +
@@ -77,29 +78,34 @@
 #'
 #' # apply alpha as constant for all logos
 #' ggplot(df, aes(x = a, y = b)) +
-#'   geom_eng_logos(aes(team_abbr = teams), width = 0.075, alpha = 0.6) +
+#'   geom_eng_logos(aes(team_name = teams), width = 0.075, alpha = 0.6) +
 #'   geom_label(aes(label = teams), nudge_y = -0.35, alpha = 0.5) +
 #'   theme_void()
 #'
 #' }
-geom_eng_logos <- function(mapping = NULL, data = NULL,
-                           stat = "identity", position = "identity",
-                           ...,
-                           na.rm = FALSE,
-                           show.legend = FALSE,
-                           inherit.aes = TRUE) {
-
+geom_soccer_logos <- function(
+    mapping = NULL,
+    data = NULL,
+    stat = 'identity',
+    position = 'identity',
+    ...,
+    na.rm = FALSE,
+    country = c('ENG', 'USA'),
+    show.legend = FALSE,
+    inherit.aes = TRUE
+) {
+  country <- rlang::arg_match(country)
   ggplot2::layer(
     data = data,
     mapping = mapping,
     stat = stat,
-    geom = GeomENGlogo,
+    geom = GeomSoccerlogo,
     position = position,
     show.legend = show.legend,
     inherit.aes = inherit.aes,
     params = list(
       na.rm = na.rm,
-      country = "ENG",
+      country = country,
       ...
     )
   )
@@ -107,21 +113,33 @@ geom_eng_logos <- function(mapping = NULL, data = NULL,
 
 #' @rdname soccerplotR-package
 #' @export
-GeomENGlogo <- ggplot2::ggproto(
-  "GeomENGlogo", ggplot2::Geom,
-  required_aes = c("x", "y", "team_abbr"),
-  # non_missing_aes = c(""),
+GeomSoccerlogo <- ggplot2::ggproto(
+  'GeomSoccerlogo', ggplot2::Geom,
+  required_aes = c('x', 'y', 'team_name'),
+  # non_missing_aes = c(''),
   default_aes = ggplot2::aes(
-    alpha = NULL, colour = NULL, angle = 0, hjust = 0.5,
-    vjust = 0.5, width = 1.0, height = 1.0
+    alpha = NULL,
+    colour = NULL,
+    angle = 0,
+    hjust = 0.5,
+    vjust = 0.5,
+    width = 1.0,
+    height = 1.0
   ),
-  draw_panel = function(data, panel_params, coord, na.rm = FALSE, country = c("ENG")) {
+  draw_panel = function(data, panel_params, coord, na.rm = FALSE, country = c('ENG', 'USA')) {
 
-    country <- match.arg(country)
+    country <- rlang::arg_match(country)
 
-    team_abbr <- clean_team_abbrs(as.character(data$team_abbr), country = country, keep_non_matches = TRUE)
+    team_name <- clean_team_name(
+      as.character(data$team_name),
+      country = country,
+      keep_non_matches = TRUE
+    )
 
-    data$path <- logo_from_abbr(team_abbr, country = country)
+    data$path <- logo_from_team_name(
+      team_name,
+      country = country
+    )
 
     ggpath::GeomFromPath$draw_panel(
       data = data,
