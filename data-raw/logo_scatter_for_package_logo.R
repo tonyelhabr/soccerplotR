dbee3c3d9251d76c330e5d04d3823e89ef57fde3
Tony ElHabr <anthonyelhabr@gmail.com>
2024-01-21 00:20:42 -0600

    better logo and social image

--- data-raw/logo_scatter_for_package_logo.R
+++ data-raw/logo_scatter_for_package_logo.R
@@ -2,6 +2,9 @@ library(hexSticker)
 library(ggplot2)
 library(soccerplotR)
 library(showtext)
+
+FONT <- 'Kanit'
+sysfonts::font_add_google(FONT)
 showtext_auto()
 
 team_names <- unlist(unname(soccerplotR::valid_team_names()))
@@ -14,6 +17,22 @@ df <- data.frame(
   team_name = sampled_team_names
 )
 
+# ## https://github.com/tidymodels/TMwR/blob/main/_common.R#L15
+# theme_transparent <- function(...) {
+#
+#   ret <- ggplot2::theme_bw(...)
+#
+#   trans_rect <- ggplot2::element_rect(fill = "transparent", colour = NA)
+#   ret$panel.background  <- trans_rect
+#   ret$plot.background   <- trans_rect
+#   ret$legend.background <- trans_rect
+#   ret$legend.key        <- trans_rect
+#
+#   ret$legend.position <- "top"
+#
+#   ret
+# }
+
 p <- ggplot(df, aes(x = a, y = b)) +
   geom_soccer_logos(aes(team_name = team_name), width = 0.09, alpha = 0.2) +
   coord_cartesian(xlim = c(0.5,7.5), ylim = c(0.5,6.5)) +
@@ -23,7 +42,7 @@ p <- ggplot(df, aes(x = a, y = b)) +
 sticker(
   p,
   package = "soccerplotR",
-  p_family = "Kanit",
+  p_family = FONT,
   # p_fontface = "regular",
   p_y = 0.6,
   p_size = 20,
@@ -45,3 +64,21 @@ sticker(
 )
 
 usethis::use_logo("data-raw/logo.png")
+
+p <- ggplot(df, aes(x = a, y = b)) +
+  geom_soccer_logos(aes(team_name = team_name), width = 0.09, alpha = 0.2) +
+  annotate("text", x = 4.5, y = 2.5, label = "soccerplotR", family = "Kanit", size = 7, color = "#ffffff") +
+  theme_void() +
+  coord_cartesian(xlim = c(0.5,8.5), ylim = c(0.5,4.5))
+
+# ggpreview(p, width = 1280, height = 640, units = "px", dpi = 600,  bg = "#222222")
+
+ggsave(
+  "man/figures/social_preview.png",
+  p,
+  width = 1280,
+  height = 640,
+  units = "px",
+  dpi = 600,
+  bg = "#222222"
+)
